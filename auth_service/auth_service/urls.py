12198c538c3621b92949ced36a66f7f65cbe69d5
Pragyo Tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-17 17:00:21 +0000

    configure projects

--- auth_service/auth_service/urls.py
+++ auth_service/auth_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for LibMgmt project.
+URL configuration for auth_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
