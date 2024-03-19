59a2106d4fb74bf896cd5c940cf7150a0fd540bf
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-19 22:02:10 +0545

    createbooks model

--- book_service/book_app/models.py
+++ book_service/book_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    summary = models.TextField()
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+    cover = models.TextField()
+
+    def __str__(self):
+        return self.title
