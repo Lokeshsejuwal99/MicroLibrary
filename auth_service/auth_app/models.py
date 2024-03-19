f55d4f286ea5790a8fc35f60cdfb362d0eed136b
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-19 22:50:57 +0545

    add recovery code and 2fa

--- auth_service/auth_app/models.py
+++ auth_service/auth_app/models.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -26,7 +29,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=50)
     recovery_code = models.CharField(max_length=255, blank=True, null=True)
-    fa_enabled = models.BooleanField(default=False)
+    twofa_enabled = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = CustomUserManager()
@@ -36,3 +39,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def save(self, *args, **kwargs):
+        if not self.recovery_code:
+            # Generate a random recovery code
+            recovery_code = "".join(
+                random.choices(string.ascii_letters + string.digits, k=12)
+            )
+            self.recovery_code = recovery_code
+        super().save(*args, **kwargs)
