1e7e06d4a1be21bbdef6e9ce4ff3a3346b0c1c19
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-20 22:32:26 +0545

    update to login viewset

--- auth_service/auth_app/views.py
+++ auth_service/auth_app/views.py
@@ -35,34 +35,49 @@ class LoginView(TokenObtainPairView):
         try:
             serializer = LoginSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
-            if serializer.validated_data["2fa"] == True:
-                print("Sending 2FA code to user")
 
             access_token = serializer.validated_data["access_token"]
             refresh_token = serializer.validated_data["refresh_token"]
             mfa = serializer.validated_data["2fa"]
 
-            # Set cookies for access and refresh tokens
-            response = Response(
-                {
-                    "success": True,
-                    "message": "User Logged in successfully",
-                    "data": {
-                        "access_token": access_token,
-                        "refresh_token": refresh_token,
-                        "2fa": mfa,
-                    },
-                },
-                status=status.HTTP_201_CREATED,
-            )
-
-            response.set_cookie("access_token", access_token, httponly=True)
-            response.set_cookie("refresh_token", refresh_token, httponly=True)
-
-            return response
+            if mfa:
+                # 2FA is enabled, return message about OTP verification
+                return self.get_2fa_response(mfa)
+            else:
+                # Set cookies only if 2FA is not enabled
+                return self.get_cookie_response(access_token, refresh_token, mfa)
 
         except Exception as e:
             return Response(
                 {"success": False, "error": "An error occurred during login."},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+    def get_2fa_response(self, mfa):
+        return Response(
+            {
+                "success": True,
+                "message": "User Logged in successfully. OTP verification required.",
+                "data": {"access_token": None, "refresh_token": None, "2fa": mfa},
+            },
+            status=status.HTTP_201_CREATED,
+        )
+
+    def get_cookie_response(self, access_token, refresh_token, mfa):
+        response = Response(
+            {
+                "success": True,
+                "message": "User Logged in successfully",
+                "data": {
+                    "access_token": access_token,
+                    "refresh_token": refresh_token,
+                    "2fa": mfa,
+                },
+            },
+            status=status.HTTP_201_CREATED,
+        )
+
+        response.set_cookie("access_token", access_token, httponly=True)
+        response.set_cookie("refresh_token", refresh_token, httponly=True)
+
+        return response
