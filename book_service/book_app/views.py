9d5d1b460e59ee6756b0b5eb6128923662f1e53b
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-21 23:04:01 +0545

    create book detail view for retrieve and update

--- book_service/book_app/views.py
+++ book_service/book_app/views.py
@@ -1,4 +1,6 @@
 from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+
 from rest_framework.response import Response
 from .models import Books
 from .serializers import BooksSerializer
@@ -34,3 +36,43 @@ class BooksView(APIView):
             {"success": False, "error": serializer.errors},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+
+class BookDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+    # lookup_field = "id"
+
+    def retrieve(self, request, user_id, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(
+            {
+                "success": True,
+                "message": "Expense retrieved successfully",
+                "data": serializer.data,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    def update(self, request, user_id, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            serializer = self.get_serializer(instance, data=request.data, partial=True)
+            if not serializer.is_valid():
+                return Response(
+                    {"success": False, "error": serializer.errors},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            serializer.save()
+            return Response(
+                {
+                    "success": True,
+                    "message": "Expense Updated successfully",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            data = {"success": False, "message": str(e)}
+            return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
