2c7058ef3c150b5e9850e6ecc4606aedd701ec53
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-24 21:39:46 +0545

    borrow book model

--- borrow_service/borrow_app/models.py
+++ borrow_service/borrow_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Borrow(models.Model):
+    user_id = models.UUIDField()  # External identifier for the user
+    book_id = models.UUIDField()  # External identifier for the book
+    borrow_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeField()
+    returned = models.BooleanField(default=False)
+    return_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.book_id + self.user_id
