ef4c9c6ddaec7363733fdb4dfe39c86820b03cca
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-24 21:59:10 +0545

    create book detial api

--- book_service/book_app/urls.py
+++ book_service/book_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BooksView
+from .views import BooksView, BookDetailView
 
 urlpatterns = [
     path("books/", BooksView.as_view(), name="books"),
+    path("book-detail/", BookDetailView.as_view(), name="BookDetail"),
 ]
