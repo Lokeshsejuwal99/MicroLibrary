53aa24e203dbe9869730ea0ee15c0fda25467c53
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-24 22:45:09 +0545

    update to book detail pi

--- book_service/book_app/urls.py
+++ book_service/book_app/urls.py
@@ -3,5 +3,5 @@ from .views import BooksView, BookDetailView
 
 urlpatterns = [
     path("books/", BooksView.as_view(), name="books"),
-    path("book-detail/", BookDetailView.as_view(), name="BookDetail"),
+    path("book-detail/<int:pk>/", BookDetailView.as_view(), name="BookDetail"),
 ]
