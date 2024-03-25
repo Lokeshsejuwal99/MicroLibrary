d5408ebbbfa90b75b7bdf7cf341ecff5cc43b57a
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-25 23:01:45 +0545

    refactor borrow model

--- borrow_service/borrow_app/models.py
+++ borrow_service/borrow_app/models.py
@@ -3,8 +3,10 @@ from django.utils import timezone
 
 
 class Borrow(models.Model):
-    user_id = models.UUIDField()  # External identifier for the user
-    book_id = models.UUIDField()  # External identifier for the book
+    # user_id = models.UUIDField()  # External identifier for the user
+    # book_id = models.UUIDField()  # External identifier for the book
+    user_id = models.IntegerField()
+    book_id = models.IntegerField()
     borrow_date = models.DateTimeField(default=timezone.now)
     due_date = models.DateTimeField()
     returned = models.BooleanField(default=False)
