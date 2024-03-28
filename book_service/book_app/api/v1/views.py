52c392c3b20f45311eb8e286a4dbca83394d1c73
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-28 22:52:35 +0545

    remove book detail view

--- book_service/book_app/api/v1/views.py
+++ book_service/book_app/api/v1/views.py
@@ -67,55 +67,55 @@ class BooksView(ModelViewSet):
         )
 
 
-class BookDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BooksSerializer
-    # lookup_field = "id"
+# class BookDetailView(RetrieveUpdateDestroyAPIView):
+#     queryset = Books.objects.all()
+#     serializer_class = BooksSerializer
+#     # lookup_field = "id"
 
-    def retrieve(self, request, user_id, *args, **kwargs):
-        try:
-            instance = self.get_object()
-            serializer = self.get_serializer(instance)
-            return Response(
-                {
-                    "success": True,
-                    "message": "Books retrieved successfully",
-                    "data": serializer.data,
-                },
-                status=status.HTTP_200_OK,
-            )
-        except Exception as e:
-            data = {"success": False, "message": str(e)}
-            return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#     def retrieve(self, request, user_id, *args, **kwargs):
+#         try:
+#             instance = self.get_object()
+#             serializer = self.get_serializer(instance)
+#             return Response(
+#                 {
+#                     "success": True,
+#                     "message": "Books retrieved successfully",
+#                     "data": serializer.data,
+#                 },
+#                 status=status.HTTP_200_OK,
+#             )
+#         except Exception as e:
+#             data = {"success": False, "message": str(e)}
+#             return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def update(self, request, user_id, *args, **kwargs):
-        try:
-            instance = self.get_object()
-            serializer = self.get_serializer(instance, data=request.data, partial=True)
-            if not serializer.is_valid():
-                return Response(
-                    {"success": False, "error": serializer.errors},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-            serializer.save()
-            return Response(
-                {
-                    "success": True,
-                    "message": "Book Updated successfully",
-                    "data": serializer.data,
-                },
-                status=status.HTTP_200_OK,
-            )
-        except Exception as e:
-            data = {"success": False, "message": str(e)}
-            return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#     def update(self, request, user_id, *args, **kwargs):
+#         try:
+#             instance = self.get_object()
+#             serializer = self.get_serializer(instance, data=request.data, partial=True)
+#             if not serializer.is_valid():
+#                 return Response(
+#                     {"success": False, "error": serializer.errors},
+#                     status=status.HTTP_400_BAD_REQUEST,
+#                 )
+#             serializer.save()
+#             return Response(
+#                 {
+#                     "success": True,
+#                     "message": "Book Updated successfully",
+#                     "data": serializer.data,
+#                 },
+#                 status=status.HTTP_200_OK,
+#             )
+#         except Exception as e:
+#             data = {"success": False, "message": str(e)}
+#             return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def destroy(self, request, user_id, *args, **kwargs):
-        try:
-            instance = self.get_object()
-            instance.delete()
-            data = {"success": True, "message": "Book deleted successfully"}
-            return Response(data, status=status.HTTP_204_NO_CONTENT)
-        except Exception as e:
-            data = {"success": False, "message": str(e)}
-            return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#     def destroy(self, request, user_id, *args, **kwargs):
+#         try:
+#             instance = self.get_object()
+#             instance.delete()
+#             data = {"success": True, "message": "Book deleted successfully"}
+#             return Response(data, status=status.HTTP_204_NO_CONTENT)
+#         except Exception as e:
+#             data = {"success": False, "message": str(e)}
+#             return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
