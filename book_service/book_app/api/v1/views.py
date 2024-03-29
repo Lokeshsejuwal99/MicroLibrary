84fe0db2b6b9987ea95b8a297b9c6ca4707b83c0
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-29 22:57:37 +0545

    handle exceptions for book service

--- book_service/book_app/api/v1/views.py
+++ book_service/book_app/api/v1/views.py
@@ -16,35 +16,65 @@ class BooksView(ModelViewSet):
     serializer_class = BooksSerializer
 
     def create(self, request, *args, **kwargs):
-        data = BookRepository.create(self.serializer_class, request.data)
-        return Response(
-            {
-                "success": True,
-                "message": "Book created successfully",
-                "data": data,  # Include serialized data of the created instance in the response
-            },
-            status=status.HTTP_201_CREATED,
-        )
+        try:
+            data = BookRepository.create(self.serializer_class, request.data)
+            return Response(
+                {
+                    "success": True,
+                    "message": "Book created successfully",
+                    "data": data,  # Include serialized data of the created instance in the response
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Error creating book",
+                    "error": str(e),  # Include the error message in the response
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
     def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        data = BookRepository.update(instance, request.data)
-        return Response(
-            {
-                "success": True,
-                "message": "Book updated successfully",
-                "data": data,  #
-            },
-            status=status.HTTP_200_OK,
-        )
+        try:
+            instance = self.get_object()
+            data = BookRepository.update(instance, request.data)
+            return Response(
+                {
+                    "success": True,
+                    "message": "Book updated successfully",
+                    "data": data,  # Include serialized data of the updated instance in the response
+                },
+                status=status.HTTP_200_OK,
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Error updating book",
+                    "error": str(e),  # Include the error message in the response
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
 
     def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        BookRepository.delete_book(instance)
-        return Response(
-            {
-                "success": True,
-                "message": "Book deleted successfully",
-            },
-            status=status.HTTP_204_NO_CONTENT,
-        )
+        try:
+            instance = self.get_object()
+            BookRepository.delete_book(instance)
+            return Response(
+                {
+                    "success": True,
+                    "message": "Book deleted successfully",
+                },
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        except Exception as e:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Error deleting book",
+                    "error": str(e),  # Include the error message in the response
+                },
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
