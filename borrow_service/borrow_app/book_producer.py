bb7a70cc1203e3e58dfe1038b60825389c2d006a
tandukar <84461389+tandukar@users.noreply.github.com>
2024-03-30 21:20:24 +0545

    reconfigure producer and consumer for handling borrow

---
+++ borrow_service/borrow_app/book_producer.py
@@ -0,0 +1,23 @@
+import pika
+import json
+from decouple import config
+
+
+def BookProducer(book):
+    print("BookProducer")
+    amqp_url = config("RABBITMQ")  # Use your environment variable for the AMQP URL
+
+    connection = pika.BlockingConnection(pika.URLParameters(amqp_url))
+    channel = connection.channel()
+
+    channel.queue_declare(queue="borrow_book_queue")
+
+    channel.basic_publish(
+        exchange="borrow_book_exchange",
+        routing_key="borrow_book_queue",
+        body=json.dumps(book),
+    )
+
+    print(f" [x] Book request published: {book}")
+
+    connection.close()
