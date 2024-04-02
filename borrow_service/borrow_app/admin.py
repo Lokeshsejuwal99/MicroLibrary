d513abdf16a9917d5919a8a4f651eb5dc66e2578
tandukar <84461389+tandukar@users.noreply.github.com>
2024-04-02 22:05:26 +0545

    Configure Poetry for package management

--- borrow_service/borrow_app/admin.py
+++ borrow_service/borrow_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Borrow
+from .models import Borrow, Books
 
 # Register your models here.
 
 admin.site.register(Borrow)
+admin.site.register(Books)
