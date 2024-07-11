5de64868b89b6f61275ac59706f1236a6e8b6fc9
Lokesh Sejuwal <102446495+Lokeshsejuwal99@users.noreply.github.com>
2024-07-11 23:09:33 +0545

    Delete fine_service directory
    Signed-off-by: Lokesh Sejuwal <102446495+Lokeshsejuwal99@users.noreply.github.com>

--- fine_service/fine_service/celery.py
+++
@@ -1,7 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fine_service.settings")
-app = Celery("fine_service")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
