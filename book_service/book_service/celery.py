82cf96091ba3db9bbd8361f9a6367c932dfec9d6
Lokesh Sejuwal <102446495+Lokeshsejuwal99@users.noreply.github.com>
2024-07-11 23:09:04 +0545

    Delete book_service directory
    Signed-off-by: Lokesh Sejuwal <102446495+Lokeshsejuwal99@users.noreply.github.com>

--- book_service/book_service/celery.py
+++
@@ -1,7 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book_service.settings")
-app = Celery("book_service")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
