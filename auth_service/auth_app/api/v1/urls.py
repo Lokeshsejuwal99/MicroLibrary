797f6c02c30792c567fa51e8915ca3849cbb475b
Lokesh Sejuwal <102446495+Lokeshsejuwal99@users.noreply.github.com>
2024-07-11 23:08:27 +0545

    Delete auth_service directory
    Signed-off-by: Lokesh Sejuwal <102446495+Lokeshsejuwal99@users.noreply.github.com>

--- auth_service/auth_app/api/v1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterView, LoginView, VerifyOTPView
-
-urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("verify-otp/", VerifyOTPView.as_view(), name="verify-otp"),
-]
